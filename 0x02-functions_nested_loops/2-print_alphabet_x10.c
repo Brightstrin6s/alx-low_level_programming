7b24516c2105afeb65db661659e4fa3274bd2d62
Brightstrin6s <ufuomaovokeufuoma@gmail.com>
2022-07-18 16:10:20 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+        int l = 'a';
+
+        while (l <= 'z')
+        {
+                l++;
+        }
+		1*=10;
+
+	_putchar(1);
+
+	_putchar('\n');
+}
