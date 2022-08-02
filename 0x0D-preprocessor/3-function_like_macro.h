b0d82941c217af42ad58c7e5ad3d5df15da9eaf6
Brightstrin6s <ufuomaovokeufuoma@gmail.com>
2022-08-01 21:14:41 -0700

    go

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
 #endif
